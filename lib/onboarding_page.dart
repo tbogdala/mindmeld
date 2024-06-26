843e59bfaf287fe29e19989f366651e65b3604c3
Timothy Bogdala <tdb@animal-machine.com>
2024-06-25 21:09:25 -0500

    hid downloading of models during onboarding if a model is being copied

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -31,6 +31,8 @@ class _OnboardingPageState extends State<OnboardingPage> {
   late String selectedAutoDLOption;
   late List<String> autoDLOptions;
 
+  bool copyingSelectedModel = false;
+
   @override
   void dispose() {
     super.dispose();
@@ -45,82 +47,102 @@ class _OnboardingPageState extends State<OnboardingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        Text('Model Setup', style: Theme.of(context).textTheme.headlineLarge),
-        const SizedBox(height: 16),
-        const Text(
-            'MindMeld needs a large language model ("LLM") neural net file in order to function. These files can be downloaded manually and imported here.'),
-        const SizedBox(height: 16),
-        FilledButton(
-          child: const Text('Import AI GGUF Model File'),
-          onPressed: () async {
-            try {
-              FilePickerResult? result = await FilePicker.platform.pickFiles(
-                  dialogTitle: "Load LLM File",
-                  type: FileType.any,
-                  allowMultiple: false,
-                  allowCompression: false);
-              if (result != null) {
-                // get the selected filepath
-                final selectedModelFilepath = result.files.first.path!;
-                final selectedModelFilename = p.basename(selectedModelFilepath);
+    if (copyingSelectedModel) {
+      return const Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: Text(
+                'Copying model file into the application. This should only take a few seconds...'),
+          ));
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Text('Model Setup', style: Theme.of(context).textTheme.headlineLarge),
+          const SizedBox(height: 16),
+          const Text(
+              'MindMeld needs a large language model ("LLM") neural net file in order to function. These files can be downloaded manually beforehand and then imported here.'),
+          const SizedBox(height: 16),
+          FilledButton(
+            child: const Text('Import AI GGUF Model File'),
+            onPressed: () async {
+              try {
+                setState(() {
+                  copyingSelectedModel = true;
+                });
 
-                // copy the file to the application's documents
-                await ConfigModelFiles.ensureModelsFolderExists();
-                final modelFolderpath =
-                    await ConfigModelFiles.getModelsFolderpath();
-                final copyModelFilepath =
-                    p.join(modelFolderpath, selectedModelFilename);
-                var originalFile = File(selectedModelFilepath);
-                await originalFile.copy(copyModelFilepath);
-                log("Copy source: $selectedModelFilepath");
-                log("Copy deset : $copyModelFilepath");
-                await FilePicker.platform.clearTemporaryFiles();
-                log("Temporary files have been cleared");
+                FilePickerResult? result = await FilePicker.platform.pickFiles(
+                    dialogTitle: "Load LLM File",
+                    type: FileType.any,
+                    allowMultiple: false,
+                    allowCompression: false);
+                if (result != null) {
+                  // get the selected filepath
+                  final selectedModelFilepath = result.files.first.path!;
+                  final selectedModelFilename =
+                      p.basename(selectedModelFilepath);
 
-                // build a new models configuration file. we no longer use the full
-                // filepath for the file and instead just use the relative one based
-                // on filename and our known models folder.
-                final configModelFiles = ConfigModelFiles(
-                    modelFiles: {selectedModelFilename: selectedModelFilename});
+                  // copy the file to the application's documents
+                  await ConfigModelFiles.ensureModelsFolderExists();
+                  final modelFolderpath =
+                      await ConfigModelFiles.getModelsFolderpath();
+                  final copyModelFilepath =
+                      p.join(modelFolderpath, selectedModelFilename);
+                  var originalFile = File(selectedModelFilepath);
+                  await originalFile.copy(copyModelFilepath);
+                  log("Copy source: $selectedModelFilepath");
+                  log("Copy deset : $copyModelFilepath");
+                  await FilePicker.platform.clearTemporaryFiles();
+                  log("Temporary files have been cleared");
 
-                log("JSON for ModelFiles: ${ConfigModelFiles.getFilepath()}");
-                final configModelFilesJson = configModelFiles.toJson();
-                log(configModelFilesJson);
+                  // build a new models configuration file. we no longer use the full
+                  // filepath for the file and instead just use the relative one based
+                  // on filename and our known models folder.
+                  final configModelFiles = ConfigModelFiles(modelFiles: {
+                    selectedModelFilename: selectedModelFilename
+                  });
 
-                // send the new data file over the callback.
-                widget.onNewConfigModelFiles(configModelFiles);
+                  log("JSON for ModelFiles: ${ConfigModelFiles.getFilepath()}");
+                  final configModelFilesJson = configModelFiles.toJson();
+                  log(configModelFilesJson);
+
+                  // send the new data file over the callback.
+                  widget.onNewConfigModelFiles(configModelFiles);
+                }
+              } catch (e) {
+                log("Got an error while trying to copy the new model file into the application's document folder: $e");
+              } finally {
+                setState(() {
+                  copyingSelectedModel = false;
+                });
               }
-            } catch (e) {
-              log("Got an error while trying to copy the new model file into the application's document folder: $e");
-            }
-          },
-        ),
-        const SizedBox(height: 32),
-        const Divider(),
-        const SizedBox(height: 32),
-        const Text(
-            "If you don't know what file to download, MindMeld can download one of these files automatically for you instead."),
-        const SizedBox(height: 16),
-        DropdownMenu(
-          initialSelection: selectedAutoDLOption,
-          dropdownMenuEntries: autoDLOptions
-              .map((option) => DropdownMenuEntry(value: option, label: option))
-              .toList(),
-          onSelected: (value) {
-            setState(() {
-              selectedAutoDLOption = value as String;
-            });
-          },
-        ),
-        const SizedBox(height: 16),
-        FilledButton(
-          child: const Text('Automatically Download Model'),
-          onPressed: () async {},
-        ),
-      ]),
-    );
+            },
+          ),
+          const SizedBox(height: 32),
+          const Divider(),
+          const SizedBox(height: 32),
+          const Text(
+              "If you don't know what file to download, MindMeld can download one of these automatically for you instead."),
+          const SizedBox(height: 16),
+          DropdownMenu(
+            initialSelection: selectedAutoDLOption,
+            dropdownMenuEntries: autoDLOptions
+                .map(
+                    (option) => DropdownMenuEntry(value: option, label: option))
+                .toList(),
+            onSelected: (value) {
+              setState(() {
+                selectedAutoDLOption = value as String;
+              });
+            },
+          ),
+          const SizedBox(height: 16),
+          FilledButton(
+            child: const Text('Automatically Download Model'),
+            onPressed: () async {},
+          ),
+        ]),
+      );
+    }
   }
 }
