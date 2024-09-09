a9c02534defee35648318bff4f9159b0c3d2891d
Timothy Bogdala <tdb@animal-machine.com>
2024-09-09 12:41:21 -0500

    * bugfix: fixed up padding on adding a new chatlog

--- lib/new_chat_log_page.dart
+++ lib/new_chat_log_page.dart
@@ -77,90 +77,95 @@ class _NewChatLogWidgetState extends State<NewChatLogWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-            'First, select an AI Model that has already been imported to use for this chatlog.'),
-        const SizedBox(height: 16),
-        DropdownMenu(
-          initialSelection: selectedModelFileOption,
-          dropdownMenuEntries: modelFileOptions
-              .map((option) => DropdownMenuEntry(value: option, label: option))
-              .toList(),
-          onSelected: (value) {
-            setState(() {
-              selectedModelFileOption = value as String;
-            });
-          },
-        ),
-        const SizedBox(height: 8),
-        const Divider(),
-        const SizedBox(height: 8),
-        const Text(
-            'Next, choose the prompt template style to use when interfacing with this model. When in doubt, check the source page for the model to see what it suggests.'),
-        const SizedBox(height: 8),
-        DropdownMenu(
-          initialSelection: selectedPromptFormatOption,
-          dropdownMenuEntries: promptFormatOptions
-              .map((option) => DropdownMenuEntry(value: option, label: option))
-              .toList(),
-          onSelected: (value) {
-            setState(() {
-              selectedPromptFormatOption = value as String;
-            });
-          },
-        ),
-        const SizedBox(height: 8),
-        const Divider(),
-        const SizedBox(height: 8),
-        const Text(
-            'Finally, personalize this chatlog by giving it a name and then press the \'Create Chatlog\' button.'),
-        const SizedBox(height: 8),
-        TextField(
-          controller: logNameController,
-          decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.account_box),
-              labelText: 'New Chatlog Name'),
-        ),
-        const SizedBox(height: 8),
-        TextField(
-          controller: userNameController,
-          decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.person), labelText: 'Your Name'),
-        ),
-        const SizedBox(height: 8),
-        TextField(
-          controller: aiNameController,
-          decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.precision_manufacturing),
-              labelText: 'AI Name'),
-        ),
-        const SizedBox(height: 32),
-        Center(
-          child: FilledButton(
-            child: const Text('Create Chatlog'),
-            onPressed: () {
-              var result = ChatLog(
-                  logNameController.text,
-                  selectedModelFileOption,
-                  modelPromptStyleFromString(selectedPromptFormatOption),
-                  '');
-              result.characters.add(ChatLogCharacter(
-                  name: userNameController.text,
-                  description: '',
-                  personality: '',
-                  isUserControlled: true));
-              result.characters.add(ChatLogCharacter(
-                  name: aiNameController.text,
-                  description: '',
-                  personality: '',
-                  isUserControlled: false));
-              Navigator.pop(context, result);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+              'First, select an AI Model that has already been imported to use for this chatlog.'),
+          const SizedBox(height: 16),
+          DropdownMenu(
+            initialSelection: selectedModelFileOption,
+            dropdownMenuEntries: modelFileOptions
+                .map(
+                    (option) => DropdownMenuEntry(value: option, label: option))
+                .toList(),
+            onSelected: (value) {
+              setState(() {
+                selectedModelFileOption = value as String;
+              });
             },
           ),
-        ),
-      ],
+          const SizedBox(height: 8),
+          const Divider(),
+          const SizedBox(height: 8),
+          const Text(
+              'Next, choose the prompt template style to use when interfacing with this model. When in doubt, check the source page for the model to see what it suggests.'),
+          const SizedBox(height: 8),
+          DropdownMenu(
+            initialSelection: selectedPromptFormatOption,
+            dropdownMenuEntries: promptFormatOptions
+                .map(
+                    (option) => DropdownMenuEntry(value: option, label: option))
+                .toList(),
+            onSelected: (value) {
+              setState(() {
+                selectedPromptFormatOption = value as String;
+              });
+            },
+          ),
+          const SizedBox(height: 8),
+          const Divider(),
+          const SizedBox(height: 8),
+          const Text(
+              'Finally, personalize this chatlog by giving it a name and then press the \'Create Chatlog\' button.'),
+          const SizedBox(height: 8),
+          TextField(
+            controller: logNameController,
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.account_box),
+                labelText: 'New Chatlog Name'),
+          ),
+          const SizedBox(height: 8),
+          TextField(
+            controller: userNameController,
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person), labelText: 'Your Name'),
+          ),
+          const SizedBox(height: 8),
+          TextField(
+            controller: aiNameController,
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.precision_manufacturing),
+                labelText: 'AI Name'),
+          ),
+          const SizedBox(height: 32),
+          Center(
+            child: FilledButton(
+              child: const Text('Create Chatlog'),
+              onPressed: () {
+                var result = ChatLog(
+                    logNameController.text,
+                    selectedModelFileOption,
+                    modelPromptStyleFromString(selectedPromptFormatOption),
+                    '');
+                result.characters.add(ChatLogCharacter(
+                    name: userNameController.text,
+                    description: '',
+                    personality: '',
+                    isUserControlled: true));
+                result.characters.add(ChatLogCharacter(
+                    name: aiNameController.text,
+                    description: '',
+                    personality: '',
+                    isUserControlled: false));
+                Navigator.pop(context, result);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
