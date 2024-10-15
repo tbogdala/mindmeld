deb755903e3b5f8d1dcefbaca606e3b8efaf7d09
Timothy Bogdala <tdb@animal-machine.com>
2024-10-15 09:03:32 -0500

    sync woolydart and update linux build script

--- make_linux_deps.sh
+++ make_linux_deps.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR=$PWD
 # Build the woolydart package for Linux
 cd packages/woolydart/src
 rm -rf build-linux
-cmake -B build-linux -DGGML_CUDA=On woolycore
-cmake --build build-linux --config Release -j 4
+cmake -B build-linux -DWOOLY_TESTS=Off -DBUILD_SHARED_LIBS=Off -DGGML_CUDA=On woolycore
+cmake --build build-linux --config Release -j8
 
 # Jump back to our project dir
 cd "$SCRIPT_DIR"
