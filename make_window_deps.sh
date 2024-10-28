47b9610589c9be5d1cc8b76cac37bd6d0e28ce88
Timothy Bogdala <tdb@animal-machine.com>
2024-10-28 19:00:34 +0000

    tweak to windows build script

--- make_window_deps.sh
+++ make_window_deps.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$PWD
 
 # Build the woolydart package for Windows
 cd packages/woolydart/src
-cmake -B build-windows -DGGML_CUDA=On -DGGML_FMA=Off -DGGML_AVX512=Off -DGGML_AVX512_VBMI=Off -DGGML_AVX512_VNNI=Off -DGGML_AVX512_BF16=Off -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=Off -DGGML_STATIC=On -DWOOLY_TESTS=Off woolycore
+cmake -B build-windows -DGGML_CUDA=On -DGGML_FMA=Off -DGGML_AVX512=Off -DGGML_AVX512_VBMI=Off -DGGML_AVX512_VNNI=Off -DGGML_AVX512_BF16=Off -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=Off -DWOOLY_TESTS=Off woolycore
 cmake --build build-windows --config Release -j 8
 
 # Jump back to our project dir
@@ -21,3 +21,7 @@ mkdir -p build/windows/x64/runner/Debug
 cp packages/woolydart/src/build-windows/Release/woolycore.dll build/windows/x64/runner/Release
 cp packages/woolydart/src/build-windows/Release/woolycore.dll build/windows/x64/runner/Debug
 
+# The app itself can now be build with a command like this
+#
+#   flutter build windows --release
+#
